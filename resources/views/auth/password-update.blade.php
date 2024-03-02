f162fcfa1254e033ce5bacfb50c01bcf98173377
Maidul <maidulweb@gmail.com>
2024-03-02 14:21:00 +0600

    reset password

--- resources/views/auth/password-update.blade.php
+++ resources/views/auth/password-update.blade.php
@@ -5,6 +5,9 @@
     <form class="max-w-md mx-auto" action="{{ route('user-password.update') }}" method="post">
         @csrf
         @method('PUT')
+        @if (session('status') == 'password-updated')
+            <p class="text-center text-green-900 font-bold">Password Updated</p>
+        @endif
         <div class="relative z-0 w-full mb-5 group">
             <input type="password" name="current_password" id="current_password"
                 class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
@@ -38,7 +41,8 @@
             @enderror
         </div>
         <button type="submit"
-            class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full  px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Register</button>
+            class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full  px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Update
+            Password</button>
     </form>
 
 
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -15,6 +15,16 @@
             @enderror
         </div>
         <div class="relative z-0 w-full mb-5 group">
+            <input type="text" name="username" id="username"
+                class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+                placeholder=" " value="{{ old('username') }}" />
+            <label for="username"
+                class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Username</label>
+            @error('username')
+                <div class="text-red-600">{{ $message }}</div>
+            @enderror
+        </div>
+        <div class="relative z-0 w-full mb-5 group">
             <input type="email" name="email" id="email"
                 class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
                 placeholder=" " value="{{ old('email') }}" />
